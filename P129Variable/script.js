2b2493d46ebfe3925c4d35ea444345d43c4d3846
HamidMammadov <hamidvm@code.edu.az>
2022-02-02 13:03:39 +0400

    First Commit

---
+++ P129Variable/script.js
@@ -0,0 +1,198 @@
+// let number1 = 5; //Reqem Tipleri
+// var txt1 = "Hello World" //Text (metn)tipi
+// var check = true//false
+// var a;
+// var b = null;
+
+// number1 = 15+30;
+
+// console.log(number1+number1);
+// console.log(a);
+// console.log(txt1+number1+check);
+// console.log(b);
+
+// let name = prompt("Adinizi Daxil Edin");
+
+// console.log("Tebrikler Adinizi Bildiniz "+name)
+
+// let a = 5;
+// let b = 7;
+
+// let result = 0;
+
+// result = a + b;
+// console.log(result)
+
+// result = a - b;
+// console.log(result);
+
+// result = a * b;
+// console.log(result);
+
+// result = a / b;
+// console.log(result);
+
+// result = a**b;
+// console.log(result);
+
+// result = b % a;
+// console.log(result);
+
+//a++
+// result = a = a + 1; //a = a+1
+
+//++a
+// result = a++;
+//result = ++a;//resul = a+1
+// console.log(result);
+// console.log(a);
+
+// result = a--; //result = a = a - 1
+// result = a
+// console.log(result)
+// console.log(a);
+
+// result = --a;
+// console.log(result);
+// console.log(a);
+
+// console.log(a++ + ++a);
+// a =a+1;
+// console.log(a + b);
+// console.log(a);
+// a = b+2
+// console.log(a);
+
+// a+=(b+2) // a = a+ (b+2)
+// console.log(a);
+// a+=b;
+// console.log(a);
+// a+=3;
+// console.log(a);
+
+// a-=(b-4) // a = a- (b-4)
+// console.log(a);
+// a-=b;
+// console.log(a);
+// a-=3;
+// console.log(a);
+
+// a*=(b-4) // a = a* (b-4)
+// console.log(a);
+// a*=b;
+// console.log(a);
+// a*=3;
+// console.log(a);
+
+// a/=(b-4) // a = a/ (b-4)
+// console.log(a);
+// a/=b;
+// console.log(a);
+// a/=3;
+// console.log(a);
+
+// a%=(b-4) // a = a% (b-4)
+// console.log(a);
+// a%=b;
+// console.log(a);
+// a%=3;
+// console.log(a);
+
+// a**=(b-4) // a = a** (b-4)
+// console.log(a);
+// a**=b;
+// console.log(a);
+// a**=3;
+// console.log(a);
+
+// console.log(a == b)
+
+// let str = "2"
+// let num = 2;
+
+// console.log(str === num);
+
+// console.log(a != b);
+
+// console.log(str !== num);
+
+// console.log(a>b);
+// console.log(b>a);
+
+// console.log(a<b);
+// console.log(b<a);
+
+// console.log(b>=a)
+// console.log(b>=7)
+// console.log(b>=9)
+
+// console.log(b<=a)
+// console.log(b<=7)
+// console.log(b<=9)
+
+// console.log(a > b ? "True Hali A="+a+" b ="+b+"-den Boyukdur" : "False Hali A="+true+" b ="+b+"-den Kicikdir")
+
+// console.log( (b**2)==14 && a<b && 7==7 && b >= 6);
+
+// console.log( (b**2)==14 || a>b || 7!=7 || b >= 6);
+
+// console.log(((b**2)!=14 || a>b || 7!=7) && b <= 6);
+
+// console.log(!(b <= a));
+// console.log(!(b >= a));
+
+// console.log(!(((b**2)!=14 || a>b || 7!=7) && b <= 6));
+// console.log(!((b**2)!=14 || a>b || 7!=7) && b <= 6);
+// console.log(((b**2)!=14 || a>b || 7!=7) && !(b <= 6));
+
+// if (a > b) 
+// {
+//     console.log("a boyukdur");
+// }
+// else if (a==b) 
+// {
+//     console.log("Beraberdir")
+// }
+// else if(a < b)
+// {
+//     console.log("b  boyukdur")
+// }
+// else
+// {
+//     console.log("Duzgun Data Daxil Et")
+// }
+
+// switch (b) {
+//     case 1:
+//         console.log("Daxil Etdiyniz Deyer Birdir")
+//         break;
+//     case 2:
+//         console.log("Daxil Etdiyniz Deyer ikidir")
+//         break;
+//     case 3:
+//         console.log("Daxil Etdiyniz Deyer Ucdur")
+//         break;
+//     case 4:
+//         console.log("Daxil Etdiyniz Deyer Dortdur")
+//         break;
+//     case 5:
+//         console.log("Daxil Etdiyniz Deyer Besdir")
+//         break;
+//     case 6:
+//         console.log("Daxil Etdiyniz Deyer altidir")
+//         break;
+//     case 7:
+//         console.log("Daxil Etdiyniz Deyer yeddidir")
+//         break;
+//     default:
+//         console.log("Duzgun Daxil Et")
+//         break;
+// }
+
+// if(b==1){
+
+// }else if(b==2){
+
+// }else if(b==3){
+
+// }
